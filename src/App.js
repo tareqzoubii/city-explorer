686b206157232f092192589060537a778b577d4e
tareqzoubii <tareqzoubi4@gmail.com>
2022-08-29 21:41:53 -0700

    V5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Main from './Main'
+import Weather from "./Weather";
+
 class App extends React.Component {
 render() {
   return (
     <div >
     <Header />
     <Main />
+    <Weather />
     <Footer />
     </div>
   )
